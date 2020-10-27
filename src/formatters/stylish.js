d59f9408d5cf29043839f4998db54f9e7b074c17
IlliaTemnov <ilyatemnov@gmail.com>
2020-10-27 22:42:51 +0200

    Fixed according to mentor`s comments. Fifth iteration

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,33 +1,35 @@
 import _ from 'lodash';
 
-const indent = (depth, tab = '  ') => tab.repeat(depth);
+const indent = (depth, num = 0) => {
+  const tab = '  ';
+  return tab.repeat(depth + num);
+};
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
-  const output = _.map(data, (value, key) => `${indent(depth + 4)}${key}: ${stringify(value, depth + 2)}`);
+  const output = _.map(data, (value, key) => `${indent(depth, 4)}${key}: ${stringify(value, depth + 2)}`);
 
-  return `{\n${output.join('\n')}\n${indent(depth + 2)}}`;
+  return `{\n${output.join('\n')}\n${indent(depth, 2)}}`;
 };
 
 const genStylishFormat = (tree) => {
   const addDepth = (nodes, depth) => {
-    const shiftedDepth = depth + 1;
     const output = nodes.flatMap((node) => {
       switch (node.type) {
         case 'nested':
-          return `${indent(shiftedDepth)}  ${node.key}: ${addDepth(node.children, depth + 2)}`;
+          return `${indent(depth, 1)}  ${node.key}: ${addDepth(node.children, depth + 2)}`;
         case 'added':
-          return `${indent(shiftedDepth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent(depth, 1)}+ ${node.key}: ${stringify(node.value, depth)}`;
         case 'removed':
-          return `${indent(shiftedDepth)}- ${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent(depth, 1)}- ${node.key}: ${stringify(node.value, depth)}`;
         case 'unchanged':
-          return `${indent(shiftedDepth)}  ${node.key}: ${stringify(node.value, depth)}`;
+          return `${indent(depth, 1)}  ${node.key}: ${stringify(node.value, depth)}`;
         case 'changed': {
           const { key, value1, value2 } = node;
-          const data1 = `${indent(shiftedDepth)}- ${key}: ${stringify(value1, depth)}`;
-          const data2 = `${indent(shiftedDepth)}+ ${key}: ${stringify(value2, depth)}`;
+          const data1 = `${indent(depth, 1)}- ${key}: ${stringify(value1, depth)}`;
+          const data2 = `${indent(depth, 1)}+ ${key}: ${stringify(value2, depth)}`;
           return [data1, data2];
         }
         default:

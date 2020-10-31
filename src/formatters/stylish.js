75762b182607dfbaac363140f8fb5332968106af
IlliaTemnov <ilyatemnov@gmail.com>
2020-10-31 12:45:10 +0200

    Fixed according to mentor`s comments. Fifth iteration.

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,42 +1,42 @@
 import _ from 'lodash';
 
-const indent = (depth, spaces = 4) => ' '.repeat(depth + spaces);
+const startIndent = 2;
+const indent = (depth, spaces = 4) => ' '.repeat(depth === 0 ? startIndent : startIndent + depth * spaces);
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
-  const output = _.map(data, (value, key) => `${indent(depth, 4)}${key}: ${stringify(value, depth + 4)}`);
+  const output = _.map(data, (value, key) => `${indent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
 
-  return `{\n${output.join('\n')}\n${indent(depth, 0)}}`;
+  return `{\n${output.join('\n')}\n${indent(depth)}  }`;
 };
 
 const genStylishFormat = (tree) => {
-  const addDepth = (nodes, depth) => {
+  const addDepth = (nodes, depth = 0) => {
     const output = nodes.flatMap((node) => {
       switch (node.type) {
         case 'nested':
-          return `${indent(depth, 0)}  ${node.key}: ${addDepth(node.children, depth + 4)}`;
+          return `${indent(depth)}  ${node.key}: {\n${addDepth(node.children, depth + 1)}\n${indent(depth)}  }`;
         case 'added':
-          return `${indent(depth, 0)}+ ${node.key}: ${stringify(node.value, depth + 2)}`;
+          return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
         case 'removed':
-          return `${indent(depth, 0)}- ${node.key}: ${stringify(node.value, depth + 2)}`;
+          return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
         case 'unchanged':
-          return `${indent(depth, 0)}  ${node.key}: ${stringify(node.value, depth + 2)}`;
+          return `${indent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
         case 'changed': {
           const { key, value1, value2 } = node;
-          const data1 = `${indent(depth, 0)}- ${key}: ${stringify(value1, depth + 2)}`;
-          const data2 = `${indent(depth, 0)}+ ${key}: ${stringify(value2, depth + 2)}`;
+          const data1 = `${indent(depth)}- ${key}: ${stringify(value1, depth)}`;
+          const data2 = `${indent(depth)}+ ${key}: ${stringify(value2, depth)}`;
           return [data1, data2];
         }
         default:
           throw new Error(`Unexpected type ${node.type}`);
       }
     });
-
-    return `{\n${output.join('\n')}\n${indent(depth - 2, 0)}}`;
+    return output.join('\n');
   };
-  return addDepth(tree, 2);
+  return `{\n${addDepth(tree)}\n}`;
 };
 
 export default genStylishFormat;
